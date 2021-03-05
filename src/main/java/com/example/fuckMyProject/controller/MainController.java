cde08bace8254df7af1e470f1bdac24452ef1afe
CMCTELECOM <thaipn309@gmail.com>
2021-03-05 14:40:59 +0700

    ccc

--- src/main/java/com/example/fuckMyProject/controller/MainController.java
+++ src/main/java/com/example/fuckMyProject/controller/MainController.java
@@ -3,14 +3,16 @@ package com.example.fuckMyProject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String root() {
-        return "index";
-    }
+//    @GetMapping("/")
+//    public String root() {
+//        return "index";
+//    }
 
     @GetMapping("/login")
     public String login(Model model) {
@@ -18,12 +20,29 @@ public class MainController {
     }
 
     @GetMapping("/admin")
-    public String admin(Model model) {
+    public String admin() {
         return "admin";
     }
 
     @GetMapping("/user")
     public String userIndex() {
         return "index";
+    }
+
+//    @RequestMapping(value = { "/", "/login" })
+//    public String login(@RequestParam(required = false) String message, final Model model) {
+//        if (message != null && !message.isEmpty()) {
+//            if (message.equals("logout")) {
+//                model.addAttribute("message", "Logout!");
+//            }
+//            if (message.equals("error")) {
+//                model.addAttribute("message", "Hey bro,Login Failed!");
+//            }
+//        }
+//        return "login";
+//    }
+    @RequestMapping("/403")
+    public String accessDenied() {
+        return "403";
     }
 }
